e2a287a7ad75e3c9634a2129a3e8e72f867a4f61
valiyev <valiyev@deloittece.com>
2024-03-06 12:00:06 +0100

    Adding the API tests

---
+++ tests/api/loginToCapitalAPIAndGetUserDetails.spec.ts
@@ -0,0 +1,45 @@
+import { test, expect } from '@playwright/test';
+// import { request } from '@playwright/test';
+
+test.describe('Login and User Details API Tests', () => {
+  let token: string;
+
+  test.beforeEach(async ({ page }, testInfo) => {
+    const response = await fetch('http://164.92.199.221/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: 'pawel.kur',
+        password: 'P!ms*4P,@7E&a',
+      }),
+    });
+
+    expect(response.status).toBe(200);
+
+    const responseData = await response.json();
+    token = responseData['x-api-token'];
+
+    testInfo.token = token;
+    console.log("Here is Token  " + token);
+  });
+
+  test('Get user details with token', async ({}, testInfo) => {
+    const token = testInfo.token;
+
+    console.log("And one more time hereee   " + token);
+
+    const response = await fetch('http://164.92.199.221/me', {
+      method: 'GET',
+      headers: {
+        'x-api-token': token,
+      },
+    });
+
+    expect(response.status).toBe(200);
+
+    const responseData = await response.json();
+    console.log(responseData);
+  });
+});
